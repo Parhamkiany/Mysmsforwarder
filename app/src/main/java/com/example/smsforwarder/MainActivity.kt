fb66841f9a4fafc066c43ef03872db86145856b0
Parhamkiany <parham.k98@gmail.com>
2025-05-22 15:28:40 +0330

    Create MainActivity.kt

---
+++ app/src/main/java/com/example/smsforwarder/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.smsforwarder
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * فعالیت اصلی که تنها یک صفحه ساده نمایش می‌دهد.
+ */
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
